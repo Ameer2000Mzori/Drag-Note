8c59ea198604a0e1170e3ebaed4b9fcddd615065
Ameer <ameerapex496@gmail.com>
2023-12-18 13:58:07 +0100

    added logic to create for each card new li and its own text

--- src/script.js
+++ src/script.js
@@ -1,35 +1,60 @@
 // selecting our elements
 var addNewBtns = document.querySelectorAll("#add-New-Btn-Id");
 // our data objects for our each card
-var backlogObj = [
+var createNewTextObj = [
     {
-        text: "what is your backlog?",
+        text: "backlog?",
     },
-];
-var progressObj = [
     {
-        text: "what you progressing?",
+        text: "progressing?",
     },
-];
-var compeleteObj = [
     {
-        text: "what did you compelete?",
+        text: "compeleted?",
     },
-];
-var onHoldObj = [
     {
-        text: "what have you on hold ?",
+        text: "on hold?",
     },
 ];
 // global varibales
 // functions
-var addNewNote = function (addNewBtn) {
+var addNewNote = function (addNewBtn, indx) {
     var ourListUl = addNewBtn.parentElement.parentElement.children[1].children[0];
     console.log(ourListUl);
+    // creating elements dynamcily :
+    var newLi = document.createElement("li");
+    newLi.textContent = "".concat(createNewTextObj[indx].text);
+    var buttonsWrap = document.createElement("div");
+    buttonsWrap.classList.add("buttons-Wrap");
+    var deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("dele-Btn");
+    var iconDeleBtn = document.createElement("i");
+    iconDeleBtn.classList.add("fa-solid", "fa-trash");
+    deleteBtn.appendChild(iconDeleBtn);
+    buttonsWrap.appendChild(deleteBtn);
+    var editBtn = document.createElement("button");
+    editBtn.classList.add("edit-Btn");
+    var iconEditBtn = document.createElement("i");
+    iconEditBtn.classList.add("fa-solid", "fa-trash");
+    editBtn.appendChild(iconEditBtn);
+    buttonsWrap.appendChild(editBtn);
+    newLi.appendChild(buttonsWrap);
+    ourListUl.appendChild(newLi);
 };
 // eventlinsters
 addNewBtns.forEach(function (addNewBtn, indx) {
     addNewBtn.addEventListener("click", function (e) {
-        addNewNote(addNewBtn);
+        addNewNote(addNewBtn, indx);
     });
 });
+// our html loop up :
+// <li>
+//   dance
+//   <div class="buttons-Wrap">
+//     <button class="dele-Btn">
+//       <i class="fa-solid fa-trash"></i>
+//     </button>
+//     <button class="edit-Btn">
+//       <i class="fa-regular fa-pen-to-square"></i>
+//     </button>
+//   </div>
+// </li>;
--- src/script.ts
+++ src/script.ts
@@ -2,41 +2,70 @@
 const addNewBtns = document.querySelectorAll("#add-New-Btn-Id");
 
 // our data objects for our each card
-let backlogObj = [
+let createNewTextObj = [
   {
-    text: "what is your backlog?",
+    text: "backlog?",
   },
-];
-
-let progressObj = [
   {
-    text: "what you progressing?",
+    text: "progressing?",
   },
-];
-
-let compeleteObj = [
   {
-    text: "what did you compelete?",
+    text: "compeleted?",
   },
-];
-
-let onHoldObj = [
   {
-    text: "what have you on hold ?",
+    text: "on hold?",
   },
 ];
 
 // global varibales
 
 // functions
-const addNewNote = (addNewBtn) => {
+const addNewNote = (addNewBtn, indx) => {
   let ourListUl = addNewBtn.parentElement.parentElement.children[1].children[0];
   console.log(ourListUl);
+
+  // creating elements dynamcily :
+  const newLi = document.createElement("li");
+  newLi.textContent = `${createNewTextObj[indx].text}`;
+
+  const buttonsWrap = document.createElement("div");
+  buttonsWrap.classList.add("buttons-Wrap");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("dele-Btn");
+  const iconDeleBtn = document.createElement("i");
+  iconDeleBtn.classList.add("fa-solid", "fa-trash");
+  deleteBtn.appendChild(iconDeleBtn);
+  buttonsWrap.appendChild(deleteBtn);
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-Btn");
+  const iconEditBtn = document.createElement("i");
+  iconEditBtn.classList.add("fa-solid", "fa-trash");
+  editBtn.appendChild(iconEditBtn);
+  buttonsWrap.appendChild(editBtn);
+
+  newLi.appendChild(buttonsWrap);
+  ourListUl.appendChild(newLi);
 };
 
 // eventlinsters
 addNewBtns.forEach((addNewBtn, indx) => {
   addNewBtn.addEventListener("click", (e) => {
-    addNewNote(addNewBtn);
+    addNewNote(addNewBtn, indx);
   });
 });
+
+// our html loop up :
+
+// <li>
+//   dance
+//   <div class="buttons-Wrap">
+//     <button class="dele-Btn">
+//       <i class="fa-solid fa-trash"></i>
+//     </button>
+//     <button class="edit-Btn">
+//       <i class="fa-regular fa-pen-to-square"></i>
+//     </button>
+//   </div>
+// </li>;

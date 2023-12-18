aef17a4f204df0b245c24ff9053ad3f1c773fa41
Ameer <ameerapex496@gmail.com>
2023-12-18 13:41:51 +0100

    added event and new comment

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 // selecting our elements
 var addNewBtns = document.querySelectorAll("#add-New-Btn-Id");
+// our data objects for our each card
 // global varibales
 // functions
 // eventlinsters
+addNewBtns.forEach(function (addNewBtn, indx) {
+    addNewBtn.addEventListener("click", function (e) {
+        console.log(e, indx);
+    });
+});
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,15 @@
 // selecting our elements
 const addNewBtns = document.querySelectorAll("#add-New-Btn-Id");
 
+// our data objects for our each card
+
 // global varibales
 
 // functions
 
 // eventlinsters
+addNewBtns.forEach((addNewBtn, indx) => {
+  addNewBtn.addEventListener("click", (e) => {
+    console.log(e, indx);
+  });
+});

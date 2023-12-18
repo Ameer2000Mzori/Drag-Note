d6458d23e49970976d40a7b3120e7b1ad3639443
Ameer <ameerapex496@gmail.com>
2023-12-18 13:44:07 +0100

    added objects needed for each card

--- src/script.js
+++ src/script.js
@@ -1,6 +1,26 @@
 // selecting our elements
 var addNewBtns = document.querySelectorAll("#add-New-Btn-Id");
 // our data objects for our each card
+var backlogObj = [
+    {
+        text: "what is your backlog?",
+    },
+];
+var progressObj = [
+    {
+        text: "what you progressing?",
+    },
+];
+var compeleteObj = [
+    {
+        text: "what did you compelete?",
+    },
+];
+var onHoldObj = [
+    {
+        text: "what have you on hold ?",
+    },
+];
 // global varibales
 // functions
 // eventlinsters
--- src/script.ts
+++ src/script.ts
@@ -2,6 +2,29 @@
 const addNewBtns = document.querySelectorAll("#add-New-Btn-Id");
 
 // our data objects for our each card
+let backlogObj = [
+  {
+    text: "what is your backlog?",
+  },
+];
+
+let progressObj = [
+  {
+    text: "what you progressing?",
+  },
+];
+
+let compeleteObj = [
+  {
+    text: "what did you compelete?",
+  },
+];
+
+let onHoldObj = [
+  {
+    text: "what have you on hold ?",
+  },
+];
 
 // global varibales
 
